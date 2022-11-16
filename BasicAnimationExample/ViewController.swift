ee675a4686d74256a023245357d656a84625b091
Mohd Hafiz <xmhafiz@gmail.com>
2022-11-16 23:09:06 +0800

    added home page

--- BasicAnimationExample/ViewController.swift
+++ BasicAnimationExample/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    enum AnimationTypes: String, CaseIterable {
+        case movement
+        case resizing
+        case fading
+        case rotation
+        case bouncing
+    }
+    let cellIdentifier = "Cell"
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(
+            UITableViewCell.self,
+            forCellReuseIdentifier: cellIdentifier
+        )
+        tableView.rowHeight = 54
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Basic Animations"
+        view.addSubview(tableView)
+        tableView.fillSuperview()
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return AnimationTypes.allCases.count
     }
 
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let title = AnimationTypes.allCases[indexPath.row]
+        let cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+        cell.accessoryType = .disclosureIndicator
+        cell.textLabel?.text = title.rawValue.capitalized
+        return cell
+    }
 }
 
